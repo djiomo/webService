cbaa817c2100737e9afb8dbb08d44441052d0d12
salaunT29 <tiborules29@hotmail.fr>
2018-02-20 18:22:19 +0100

    Add files via upload

---
+++ SmartWCFClient.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartWCFClient.WpfApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            ServiceReference1.Service1Client test = new ServiceReference1.Service1Client();
+            MethodInfo[] methods = typeof(ServiceReference1.IService1).GetMethods();
+            Uri WebServiceUrl = test.Endpoint.Address.Uri;
+            TreeViewItem wsurl = new TreeViewItem();
+            wsurl.Header = WebServiceUrl;
+            TreeViewWebService.Items.Add(wsurl);
+            /*foreach (var m in methods)
+            {
+                MethodesListBox.Items.Add(m.Name);        
+                ParameterInfo[] listParam = m.GetParameters();
+                wsurl.Items.Add(m.Name);
+            }*/
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //LabelResult.Visibility = Visibility.Visible;
+            //LabelMethodResult.Visibility = Visibility.Visible;
+            ServiceReference1.IService1 test = new ServiceReference1.Service1Client();
+            var method = typeof(ServiceReference1.IService1).GetMethod(MethodesListBox.SelectedItem.ToString());
+            ParameterInfo[] param = typeof(ServiceReference1.IService1).GetMethod(MethodesListBox.SelectedItem.ToString()).GetParameters();
+            object[] givenParamInit = null;
+            try
+            {
+                givenParamInit = new object[] { };
+                int compteur=0;
+                /*foreach (TextBox p in StackPanelMiddleCenter.Children)
+                {
+                    object param1 = Convert.ChangeType(p.Text, param[compteur].ParameterType);
+                    object[] givenParam = new object[givenParamInit.Length + 1];
+                    givenParamInit.CopyTo(givenParam, 0);
+                    givenParam[givenParam.Length-1] = param1;
+                    givenParamInit = givenParam;
+                    compteur++;
+                }*/
+                object result = method.Invoke(test, givenParamInit);
+                //LabelMethodResult.Content = result.ToString();
+            }
+            catch (Exception exc)
+            {
+                //LabelResult.Content = "Error";
+                //LabelMethodResult.Content = exc.ToString();
+            }
+        }
+
+        private void OnSelected(object sender, SelectionChangedEventArgs e)
+        {
+            //StackPanelMiddleLeft.Children.Clear();
+            //StackPanelMiddleCenter.Children.Clear();
+            ListBox lb = e.Source as ListBox;
+            if(lb.SelectedItem != null)
+            {
+                ServiceReference1.IService1 test = new ServiceReference1.Service1Client();
+                ParameterInfo[] param = typeof(ServiceReference1.IService1).GetMethod(lb.SelectedItem.ToString()).GetParameters();
+                if (param.Length>0)
+                {
+                    foreach (var p in param)
+                    {
+                        Label labelParamType = new Label();
+                        labelParamType.HorizontalAlignment = HorizontalAlignment.Center;
+                        TextBox textBoxValue = new TextBox();
+                        textBoxValue.Height = 25;
+                        labelParamType.Content = p.ToString();
+                        //StackPanelMiddleLeft.Children.Add(labelParamType);
+                        //StackPanelMiddleCenter.Children.Add(textBoxValue);
+                    }
+                }
+                else
+                {
+                    Label noParam = new Label();
+                    noParam.Content = "Pas de paramètres pour cette fonction";
+                    //StackPanelMiddleLeft.Children.Add(noParam);
+                }
+            }
+            else
+            {
+                Console.WriteLine("item nul");
+            }
+        }
+
+        private void TreeViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var tViewItem = sender as TreeViewItem;
+            TabItem tItem = new TabItem();
+            tItem.Header = tViewItem.Header;
+            MethodInfo[] methods = typeof(ServiceReference1.IService1).GetMethods();
+            var methodeSelectionnee = methods.Where(m => m.Name.Equals(tViewItem.Header));
+            var premiereMethode = methodeSelectionnee.First();
+            ParameterInfo[] parametres = premiereMethode.GetParameters();
+            List<MyParameterInfo> listMpi = parametres.Select(p => new MyParameterInfo(p)).ToList();
+            UserControl user = new UserControl1(listMpi);
+
+            tItem.Content = user;
+            TabControlDemarrage.Items.Add(tItem);
+            TabControlDemarrage.Items.Refresh();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Window WindowURL = new Window1();
+            WindowURL.ShowDialog();
+        }
+
+        private void URLOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TreeViewItemProjets_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+            Window WindowURL = new Window1();
+            WindowURL.ShowDialog();
+            GridTabControlDemarrage.Children.Clear();
+           
+        }
+    }
+}
---
+++ SmartWCFClient.WpfApp/Window1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+using System.Web.Services;
+using System.Web.Services.Description;
+using System.CodeDom;
+using System.Text;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Net;
+
+namespace SmartWCFClient.WpfApp
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                Console.WriteLine(currentDirectory);
+                Uri webServiceUrl = new Uri(ComboBoxURL.Text.ToString());
+                ComboBoxURL.Items.Add(webServiceUrl);
+                Console.WriteLine("URL valide");
+                WebClient wc = new WebClient();
+                String path = "ws2.xml";
+                wc.DownloadFile(webServiceUrl,path);
+                Console.WriteLine("Téléchargement valide");
+                ServiceDescription wsdlDescription = ServiceDescription.Read(path);
+                Console.WriteLine("1");
+                ServiceDescriptionImporter wsdlImporter = new ServiceDescriptionImporter();
+                Console.WriteLine("2");
+                //Vérifier si le protocole d'accès au webservice est Soap12
+                wsdlImporter.ProtocolName = "Soap12";
+                Console.WriteLine("3");
+                wsdlImporter.AddServiceDescription(wsdlDescription, null, null);
+                Console.WriteLine("4");
+                wsdlImporter.Style = ServiceDescriptionImportStyle.Server;
+                Console.WriteLine("5");
+                wsdlImporter.CodeGenerationOptions = System.Xml.Serialization.CodeGenerationOptions.GenerateProperties;
+                Console.WriteLine("6");
+                CodeNamespace codeNamespace = new CodeNamespace();
+                Console.WriteLine("7");
+                CodeCompileUnit codeUnit = new CodeCompileUnit();
+                Console.WriteLine("8");
+                codeUnit.Namespaces.Add(codeNamespace);
+                Console.WriteLine("9");
+                ServiceDescriptionImportWarnings importWarnings = wsdlImporter.Import(codeNamespace, codeUnit);
+                Console.WriteLine("10");
+
+                if (importWarnings ==0)
+                {
+                    Console.WriteLine("11");
+                    StringBuilder stringBuilder = new StringBuilder();
+                    Console.WriteLine("12");
+                    StringWriter stringWriter = new StringWriter(stringBuilder);
+                    Console.WriteLine("13");
+                    CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+                    Console.WriteLine("14");
+                    provider.GenerateCodeFromCompileUnit(codeUnit, stringWriter, new CodeGeneratorOptions());
+                    Console.WriteLine("15");
+                    stringWriter.Close();
+                    Console.WriteLine("16");
+                    //Indiquer le chemin de sortie du ws genere en local
+                    File.WriteAllText("Myrandomservice.cs", stringBuilder.ToString(), Encoding.UTF8);
+                    Console.WriteLine("17");
+                }
+                else
+                {
+                    Console.WriteLine(importWarnings);
+                }
+            }
+
+            catch (UriFormatException exception)
+            { System.Windows.Forms.MessageBox.Show("URL invalide. Veuillez entrer une adresse valide"); }
+        }
+    }
+}
